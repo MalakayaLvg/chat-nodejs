7366483d4bfa360f5f09dfbaa55eac15eb7cdffc
Oslynx <malakayalvg@gmail.com>
2024-11-08 15:49:11 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,17 @@
+const socket = io('ws://chatserver.malakayalauvergnat.com');
+const button = document.querySelector("button");
+
+socket.on('message', (message) => {
+    console.log(message)
+    const ligne = document.createElement('li')
+    ligne.innerHTML = message;
+    document.querySelector('ul').appendChild(ligne);
+})
+
+document.querySelector('button').addEventListener('click', (e) => {
+    const toSend = document.querySelector('input').value;
+    socket.emit('message', {
+        content:toSend,
+    });
+
+})
---
+++ server/index.js
@@ -0,0 +1,49 @@
+
+const http = require('http');
+const socketIo = require('socket.io')
+
+const server = http.createServer((req,res) => {
+
+    res.writeHead(200, {'Content-type': "text/plain" });
+    res.end("serveur socket.io en fonctionnement");
+
+})
+
+const io = socketIo(server, {
+    transport: ["websocket","polling"],
+    cors: {
+        origin: "https://chatclient.esdlyon.dev",
+        methods: ["GET","POST"],
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('New user connect to the server');
+    console.log('user-Socker ID :'+ socket.id);
+
+
+
+    socket.on('message', (message) => {
+        console.log('Received message from ' + socket.id);
+        console.log(message.content);
+
+        try{
+            io.emit('message',{
+                author : socket.id,
+                content: message.content,
+            });
+        }catch (e) {
+            console.log(e);
+        }finally {
+            console.log('Broadcasted message to all clients');
+            console.log(io.sockets.sockets.size);
+        }
+    })
+
+})
+
+server.listen(8080, ()=>{
+    console.log('Server listening on port 8080');
+});
+

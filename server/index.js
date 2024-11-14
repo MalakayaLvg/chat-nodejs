a8a6bc99a10673276959f861a2ba9aec6419bd00
Oslynx <malakayalvg@gmail.com>
2024-11-14 21:38:55 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,49 +1,62 @@
-
+const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io')
+const socketIo = require('socket.io');
+const mongoose = require("mongoose");
+const path = require("path");
 
-const server = http.createServer((req,res) => {
+const MONGODB_URI = "mongodb://127.0.0.1:27017/chatDB";
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('✅ Successfully connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.error('🔴 Failed to connect to MongoDB:', err.message);
+    });
 
-    res.writeHead(200, {'Content-type': "text/plain" });
-    res.end("serveur socket.io en fonctionnement");
-
-})
-
+const app = express();
+const server = http.createServer(app);
 const io = socketIo(server, {
-    transport: ["websocket","polling"],
+    transports: ['websocket', 'polling'],
     cors: {
-        origin: "https://chatclient.malakayalauvergnat.com",
-        methods: ["GET","POST"],
+        origin: "*",
+        methods: ['GET', 'POST'],
     }
 });
 
+const Message = require('./models/Message');
 
 io.on('connection', (socket) => {
-    console.log('New user connect to the server');
-    console.log('user-Socker ID :'+ socket.id);
+    console.log('New user connected');
+    console.log('user-Socket ID: ' + socket.id);
 
-
+    Message.find().sort().then(messages => {
+        socket.emit('previousMessages', messages);
+    });
 
     socket.on('message', (message) => {
-        console.log('Received message from ' + socket.id);
+        console.log('Received message : '+ socket.id)
         console.log(message.content);
 
-        try{
-            io.emit('message',{
-                author : socket.id,
-                content: message.content,
-            });
-        }catch (e) {
-            console.log(e);
-        }finally {
-            console.log('Broadcasted message to all clients');
-            console.log(io.sockets.sockets.size);
-        }
-    })
+        const newMessage = new Message({
+            content: message.content
+        });
 
-})
+        newMessage.save()
+            .then(() => {
+                io.emit('message', {
+                    author: socket.id,
+                    content: message.content,
+                });
+                console.log('Message saved to database');
+            })
+            .catch(err => console.error('Failed to save message:', err));
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
 
 server.listen(8080, ()=>{
     console.log('Server listening on port 8080');
 });
-
